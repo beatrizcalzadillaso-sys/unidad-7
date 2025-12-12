2aaef4a5e4e1e439b0fad84af801e98bae5b2d8b
beatrizcalzadillaso <beatriz.calzadillaso@elorrieta-errekamari.com>
2025-12-12 20:58:43 +0100

    dk

--- queries_empresa2.sql
+++ queries_empresa2.sql
@@ -1,1 +1,7 @@
 use empresa2_dam;
+
+/*a) Muestra por cada empleado su código, nombre, salario y el nombre del departamento en el que
+trabaja.*/
+
+select codemp, nomemp, salemp, nomdep
+from empleado natural join departamento
