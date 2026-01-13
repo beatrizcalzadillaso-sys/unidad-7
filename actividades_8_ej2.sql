5d33a0a552d5a429a3f57b0ce66b710a41cfb6b1
beatrizcalzadillaso <beatriz.calzadillaso@elorrieta-errekamari.com>
2026-01-13 21:12:37 +0100

    new ej2

---
+++ actividades_8_ej2.sql
@@ -0,0 +1,18 @@
+use empresa2_dam;
+
+select *
+from centro;
+
+/*a) Muestra por cada centro de trabajo, su nombre y dirección, así como el número de
+departamentos en que se divide y el presupuesto máximo y mínimo de sus departamentos.*/
+select c.nomcen 'NomCentro', c.dircen 'DirCentro', d.nomdep 'NomDep', count(d.codcen)
+from centro c join departamento d on c.codcen=d.codcen
+group by nomcen;
+
+
+
+/*b) Muestra por cada habilidad su código y descripción, el número de empleados que la poseen y
+el nivel medio de dicha habilidad redondeado a un decimal.*/
+
+/*c) Muestra por cada departamento su nombre, presupuesto, el nombre del departamento del que
+depende y el presupuesto de este. Asigna alias a todos los atributos*/
