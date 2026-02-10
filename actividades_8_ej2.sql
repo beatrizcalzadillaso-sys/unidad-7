2c9eb055c99babd002a0d190bfbd22f1c14a06e4
beatrizcalzadillaso <beatriz.calzadillaso@elorrieta-errekamari.com>
2026-02-10 19:48:17 +0100

    b y c

--- actividades_8_ej2.sql
+++ actividades_8_ej2.sql
@@ -5,13 +5,10 @@ from centro;
 
 /*a) Muestra por cada centro de trabajo, su nombre y dirección, así como el número de
 departamentos en que se divide y el presupuesto máximo y mínimo de sus departamentos.*/
-select c.nomcen 'NomCentro', c.dircen 'DirCentro', count(d.nomdep)'CantidadDepartamentos', max(d.preanu), min(preanu)
-from centro c join departamento d on c.codcen=d.codcen
-group by c.nomcen;
 
-select count(d.nomdep)'CantidadDepartamentos'
+select count(d.nomdep)'CantidadDepartamentos', c.nomcen 'NomCentro', c.dircen 'DirCentro', max(d.preanu) 'Presupesto max', min(preanu) 'Presupuesto minimo'
 from centro c join departamento d on c.codcen=d.codcen
-group by c.nomcen;
+group by c.nomcen, c.dircen; /*hubo que agrupar por c.dircen tambien para que saliera la consulta*/
 
 select *
 from centro c join departamento d on c.codcen=d.codcen;
@@ -21,5 +18,20 @@ from centro c join departamento d on c.codcen=d.codcen;
 /*b) Muestra por cada habilidad su código y descripción, el número de empleados que la poseen y
 el nivel medio de dicha habilidad redondeado a un decimal.*/
 
+select *
+from habilidad H join habemp HE on H.codhab=HE.codhab;
+
+select count(h.codhab) 'CantEmpconHab', h.codhab 'CodHab', h.deshab 'DescHab', round(avg(he.nivhab),2) 'PromedioHab'
+from habilidad H join habemp HE on H.codhab=HE.codhab
+group by h.codhab;
+
+
+
 /*c) Muestra por cada departamento su nombre, presupuesto, el nombre del departamento del que
 depende y el presupuesto de este. Asigna alias a todos los atributos*/
+
+select nomdep, preanu, coddepdep
+from departamento;
+
+select D.nomdep'NombreDep', D.preanu'PresupuestoAnual', DP.Nomdep 'DepDelQueDepende', DP.preanu 'PreAnu_del_DepDelQueDepende' 
+from departamento D join departamento DP on D.coddepdep = DP.coddep;
